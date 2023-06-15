924e9cc7323b132a27857a3784d72ffa6183a058
dingjf <1325560831@qq.com>
2023-06-15 16:15:01 +0800

    Changes

--- chapter02/003.tdd/main.go
+++ chapter02/003.tdd/main.go
@@ -11,21 +11,22 @@ func inputRecord(name string, fatRate float64) {
 }
 
 func getRand(name string) (rank int, fatRate float64) {
-	fatRate2personMap := map[float64]string{}
+	fatRate2personMap := map[float64][]string{}
 	rankArr := make([]float64, 0, len(personFatRate))
 	for nameItem, frItem := range personFatRate {
-		fatRate2personMap[frItem] = nameItem
+		fatRate2personMap[frItem] = append(fatRate2personMap[frItem], nameItem)
 		rankArr = append(rankArr, frItem)
 	}
 	sort.Float64s(rankArr)
 	for i, frItem := range rankArr {
-		_name := fatRate2personMap[frItem]
-		if _name == name {
-			rank = i + 1
-			fatRate = frItem
-			return
+		_names := fatRate2personMap[frItem]
+		for _, _names := range _names {
+			if _names == name {
+				rank = i + 1
+				fatRate = frItem
+				return
+			}
 		}
-
 	}
 	//if name == "王强" {
 	//	return 1, 0.32
--- chapter02/003.tdd/main_test.go
+++ chapter02/003.tdd/main_test.go
@@ -49,3 +49,36 @@ func TestCase1(t *testing.T) {
 	}
 
 }
+
+func TestCase2(t *testing.T) {
+	inputRecord("王强", 0.38) //录入
+	inputRecord("张伟", 0.38)
+	inputRecord("李静", 0.28)
+	{
+		randOfLJ, fatRandOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第一，但是得到的是:%d", randOfLJ)
+		}
+		if fatRandOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂是0.28，但得到的是:%f", fatRandOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRandOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第一，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂是0.38，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+	{
+		randOfZW, fatRandOfZW := getRand("张伟")
+		if randOfZW != 2 {
+			t.Fatalf("预期张伟第一，但是得到的是:%d", randOfZW)
+		}
+		if fatRandOfZW != 0.38 {
+			t.Fatalf("预期张伟的体脂是0.38，但得到的是:%f", fatRandOfZW)
+		}
+	}
+}
