f50366e1c3887f071279ea240e48b1f9349d07d5
dingjf <1325560831@qq.com>
2023-06-17 18:09:23 +0800

    changes

---
+++ chapter04/05-objectrank/main.go
@@ -0,0 +1,76 @@
+package _03_tdd
+
+import (
+	"math"
+	"sort"
+)
+
+type RankItem struct {
+	Name    string
+	FatRate float64
+}
+type FatRateRank struct {
+	items []RankItem
+}
+
+func (r *FatRateRank) inputRecord(name string, fatRate ...float64) {
+	minFatRate := math.MaxFloat64
+	for _, item := range fatRate {
+		if minFatRate > item {
+			minFatRate = item
+		}
+	}
+	found := false
+	for i, item := range r.items {
+		if item.Name == name {
+			if item.FatRate >= minFatRate {
+				item.FatRate = minFatRate
+			}
+			r.items[i] = item
+			found = true
+			break
+		}
+	}
+	if !found {
+		r.items = append(r.items, RankItem{
+			Name:    name,
+			FatRate: minFatRate,
+		})
+	}
+}
+
+func (r *FatRateRank) getRand(name string) (rank int, fatRate float64) {
+	sort.Slice(r.items, func(i, j int) bool {
+		return r.items[i].FatRate < r.items[j].FatRate
+	})
+	frs := map[float64]struct{}{}
+
+	for _, item := range r.items {
+		frs[item.FatRate] = struct{}{}
+		if item.Name == name {
+			fatRate = item.FatRate
+		}
+	}
+	rankArr := make([]float64, 0, len(frs))
+	for k := range frs {
+		rankArr = append(rankArr, k)
+	}
+	sort.Float64s(rankArr)
+	for i, frItem := range rankArr {
+		if frItem == fatRate {
+			rank = i + 1
+			break
+		}
+	}
+
+	//for nameItem, frItem := range personFatRate {
+	//	fatRate2personMap[frItem] = append(fatRate2personMap[frItem], nameItem)
+	//	rankArr = append(rankArr, frItem)
+	//}
+	//sort.Float64s(rankArr)
+
+	////if name == "王强" {
+	////	return 1, 0.32
+	//}
+	return
+}
---
+++ chapter04/05-objectrank/main_test.go
@@ -0,0 +1,119 @@
+package _03_tdd
+
+import "testing"
+
+func TestCase1Part1(t *testing.T) {
+	r := &FatRateRank{}
+	r.inputRecord("王强", 0.38) //录入
+	r.inputRecord("王强", 0.32)
+	{
+		randOfWQ, fatRandOfWQ := r.getRand("王强")
+		if randOfWQ != 1 {
+			t.Fatalf("预期王强第一，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.32 {
+			t.Fatalf("预期王强的体脂是0.32，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+}
+
+func TestCase1(t *testing.T) {
+	r := &FatRateRank{}
+	r.inputRecord("王强", 0.38) //录入
+	r.inputRecord("王强", 0.32)
+	{
+		randOfWQ, fatRandOfWQ := r.getRand("王强")
+		if randOfWQ != 1 {
+			t.Fatalf("预期王强第一，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.32 {
+			t.Fatalf("预期王强的体脂是0.32，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+	r.inputRecord("李静", 0.28)
+	{
+		randOfWQ, fatRandOfWQ := r.getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第2，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.32 {
+			t.Fatalf("预期王强的体脂是0.32，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+	{
+		randOfLJ, fatRandOfLJ := r.getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第1，但是得到的是:%d", randOfLJ)
+		}
+		if fatRandOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂是0.28，但得到的是:%f", fatRandOfLJ)
+		}
+	}
+
+}
+
+func TestCase2(t *testing.T) {
+	r := &FatRateRank{}
+
+	r.inputRecord("王强", 0.38) //录入
+	r.inputRecord("张伟", 0.38)
+	r.inputRecord("李静", 0.28)
+	{
+		randOfLJ, fatRandOfLJ := r.getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第一，但是得到的是:%d", randOfLJ)
+		}
+		if fatRandOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂是0.28，但得到的是:%f", fatRandOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRandOfWQ := r.getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第2，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂是0.38，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+	{
+		randOfZW, fatRandOfZW := r.getRand("张伟")
+		if randOfZW != 2 {
+			t.Fatalf("预期张伟第2，但是得到的是:%d", randOfZW)
+		}
+		if fatRandOfZW != 0.38 {
+			t.Fatalf("预期张伟的体脂是0.38，但得到的是:%f", fatRandOfZW)
+		}
+	}
+}
+func TestCase3(t *testing.T) {
+	r := &FatRateRank{}
+
+	r.inputRecord("王强", 0.38) //录入
+	r.inputRecord("李静", 0.28)
+	r.inputRecord("张伟")
+	{
+		randOfLJ, fatRandOfLJ := r.getRand("李静")
+		if randOfLJ != 1 {
+			t.Fatalf("预期李静第一，但是得到的是:%d", randOfLJ)
+		}
+		if fatRandOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂是0.28，但得到的是:%f", fatRandOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRandOfWQ := r.getRand("王强")
+		if randOfWQ != 2 {
+			t.Fatalf("预期王强第2，但是得到的是:%d", randOfWQ)
+		}
+		if fatRandOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂是0.38，但得到的是:%f", fatRandOfWQ)
+		}
+	}
+	{
+		randOfZW, _ := r.getRand("张伟")
+		if randOfZW != 3 {
+			t.Fatalf("预期张伟第三，但是得到的是:%d", randOfZW)
+		}
+	}
+}
