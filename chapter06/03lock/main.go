9bfb90e1f86400cb7b29176682fa861779027bc8
dingjf <1325560831@qq.com>
2023-07-02 10:46:49 +0800

    changes

---
+++ chapter06/03lock/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		//countDict()
+		//countDictGoroutineSafe()
+		//countDictGoroutineForgetUnlock()
+		countDictLockPrice()
+	}
+
+}
+
+func countDict() {
+	totalNum := 0
+	wg := sync.WaitGroup{}
+	wg.Add(5000)
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer wg.Done()
+			totalNum += 100 //totalNum=totalNum+100//注意这里有重复覆盖的问题
+		}()
+	}
+	wg.Wait()
+	fmt.Println("预计有：", 100*5000)
+	fmt.Println("总字数：", totalNum)
+}
+
+func countDictGoroutineSafe() {
+	totalNum := 0
+	totalNumlock := sync.Mutex{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(5000)
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer wg.Done()
+			totalNumlock.Lock()
+			totalNum += 100 //totalNum=totalNum+100//注意这里有重复覆盖的问题
+			totalNumlock.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("预计有：", 100*5000)
+	fmt.Println("总字数：", totalNum)
+}
+func countDictGoroutineForgetUnlock() {
+	totalNum := 0
+	totalNumlock := sync.Mutex{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(5000)
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer wg.Done()
+			totalNumlock.Lock()
+			defer totalNumlock.Unlock()
+
+			totalNum += 100 //totalNum=totalNum+100//注意这里有重复覆盖的问题
+			//totalNumlock.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("预计有：", 100*5000)
+	fmt.Println("总字数：", totalNum)
+}
+func countDictLockPrice() {
+	totalNum := 0
+	totalNumlock := sync.Mutex{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func(pageNum int) {
+			defer wg.Done()
+
+			totalNumlock.Lock()
+			totalNum += 100 //totalNum=totalNum+100//注意这里有重复覆盖的问题
+
+			if pageNum == 3 {
+				time.Sleep(3 * time.Second)
+			}
+			totalNumlock.Unlock()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("预计有：", 100*5000)
+	fmt.Println("总字数：", totalNum)
+}
