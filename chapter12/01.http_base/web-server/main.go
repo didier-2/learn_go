1129ae9684a4acb14debbe43d47263e9e5f0917e
dingjf <1325560831@qq.com>
2023-08-06 19:22:17 +0800

    从url提取数据 http://localhost:8088/?name=xiaoqiang,sex=%E7%94%B7

--- chapter12/01.http_base/web-server/main.go
+++ chapter12/01.http_base/web-server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
 )
 
 func main() {
 	http.ListenAndServe(":8088", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		time.Sleep(2 * time.Second)
+		//time.Sleep(2 * time.Second)
 
-		writer.Write([]byte("hello，你好"))
+		qp := request.URL.Query()
+		data, _ := json.Marshal(qp)
+		writer.Write([]byte("hello，你好" + string(data)))
 	}))
 }
