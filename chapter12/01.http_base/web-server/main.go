a32164bfe8744e36ce93a45d11bbd1a1456d5b5e
dingjf <1325560831@qq.com>
2023-08-08 17:21:06 +0800

    使用servMux实现路由分发 *分发不同的请求 *实现不同的返回 *没有注册的路由，默认是404 *switch/case来区分不同的method

--- chapter12/01.http_base/web-server/main.go
+++ chapter12/01.http_base/web-server/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8088", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		//time.Sleep(2 * time.Second)
+	m := http.NewServeMux()
+	m.Handle("/hallo", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`hallo`))
+	}))
+	m.Handle("/rank", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`rank`))
+	}))
+	m.Handle("/history/xiaoqiang", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`history/xiaoqiang`))
+	}))
+	m.Handle("/history/xiaoding", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(`history/xiaoding`))
+	}))
+	m.Handle("/history", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 
 		qp := request.URL.Query()
-		data, _ := json.Marshal(qp)
-		writer.Write([]byte("hello，你好" + string(data)))
+		name := qp.Get("name")
+		//if/else
+		//switch/case
+		//switch request.Method {
+		//case "get"://...
+		//case "post"://...
+		//}
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(fmt.Sprintf(`%s,%s的健身房`, request.Method, name)))
 	}))
+
+	http.ListenAndServe(":8080", m)
 }
