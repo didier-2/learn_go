e9cc12135dbd5e626f4e29c259b04e26d3a701d0
dingjf <1325560831@qq.com>
2023-09-16 11:45:25 +0800

    grpc

---
+++ chapter13/02grpc/server/frinterface/interfaca.go
@@ -0,0 +1,16 @@
+package frinterface
+
+import (
+	"go.learn/pkg/apis"
+)
+
+type ServerInterface interface {
+	RegisterPersonalInformation(pi *apis.PersonalInformation) error
+	UpdatePersonalInformation(pi *apis.PersonalInformation) (*apis.PersonalInformationFatRate, error)
+	GetFatRate(name string) (*apis.PersonalRank, error)
+	GetTop() ([]*apis.PersonalRank, error)
+}
+
+type RankInitInterface interface {
+	Init() error
+}
